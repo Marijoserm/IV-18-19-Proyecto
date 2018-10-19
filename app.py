5943cde5621a4601c6e803a2d58ecf871ec458ab
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-10-19 11:58:57 +0200

    Subo app

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from datetime import datetime
+from pdfupload import PDFUpload
 
 app = Flask(__name__)
+pdfupload = PDFUpload()
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def home():
+    return "Hello world"
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+@app.route('/status')
+def status():
+    return pdfupload.Status()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
