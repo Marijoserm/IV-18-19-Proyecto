ad0ed4e76850f7a862fcfddf3dc817914cb67fc0
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-10-22 11:43:16 +0200

    Actualizo app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ app = Flask(__name__, template_folder='./templates/')
 pdfupload = PDFUpload()
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/status')
 def status():
     return pdfupload.Status()
+    
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
